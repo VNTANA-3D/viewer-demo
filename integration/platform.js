b0b8e6fe5bbfe1660c3a4f93a8186674424af62c
Neven Krajina <neven.krajina@gmail.com>
2024-04-10 19:25:51 +0200

    initial commit

---
+++ integration/platform.js
@@ -0,0 +1,27 @@
+export async function getPlatformData(organization, workspace, product) {
+  const baseUrl = `https://api.vntana.com`;
+  const productUrl = `${baseUrl}/products/${product}/organizations/${organization}/clients/${workspace}`
+
+  const productData = await fetch(productUrl).then(response => response.json());
+
+  if (productData.errors.length > 0) {
+    throw new Error(productData.errors);
+  }
+
+  const models = productData.response.asset.models;
+  const glbId = models.find(model => model.conversionFormat === "GLB")?.modelBlobId || "";
+  const usdzId = models.find(model => model.conversionFormat === "USDZ")?.modelBlobId || "";
+
+  const src = `${baseUrl}/assets/products/${product}/organizations/${organization}/clients/${workspace}/${glbId}`;
+  const usdzSrc = `${baseUrl}/assets/products/${product}/organizations/${organization}/clients/${workspace}/${usdzId}`;
+  const poster = `${baseUrl}/assets/thumbnail/products/${product}/organizations/${organization}/clients/${workspace}/`;
+  const qrUrl = `https://embed.vntana.com?productUuid=${product}&clientSlug=${workspace}&organizationSlug=${organization}&autoAR=true`;
+
+  return {
+    src,
+    usdzSrc,
+    poster,
+    qrUrl,
+    config: JSON.parse(productData.response.viewerSettings.value),
+  };
+}
